77b89e787e4e7029127829c0ff30599cec23dc7a
Max Seelemann <max@the-soulmen.com>
2014-04-25 10:17:28 +0200

    Textual implementation of text storage with attribute caching.

--- TextKitEditor/TKETextStorage.h
+++ TextKitEditor/TKETextStorage.h
@@ -6,8 +6,16 @@
 //  Copyright (c) 2014 Max Seelemann. All rights reserved.
 //
 
+@class TKECodeString;
+
 /*!
  @abstract Text storage performing syntax highlighting with the use of a TKECodeString.
  */
 @interface TKETextStorage : NSTextStorage
+
+/*!
+ @abstract The code string to be used as the text storage's contents.
+ */
+@property(nonatomic) TKECodeString *content;
+
 @end
--- TextKitEditor/TKETextStorage.m
+++ TextKitEditor/TKETextStorage.m
@@ -8,6 +8,89 @@
 
 #import "TKETextStorage.h"
 
+#import "TKECodeString.h"
+
+
 @implementation TKETextStorage
+{
+	// Cache with precomupted attributes used for performance and convenience.
+	NSMutableAttributedString *_cache;
+}
+
+
+- (id)init
+{
+    self = [super init];
+    
+    if (self) {
+        _cache = [NSMutableAttributedString new];
+    }
+	
+    return self;
+}
+
+
+#pragma mark - Properties
+
+- (void)setContent:(TKECodeString *)content
+{
+	_content = content;
+	
+	// Coalesce change notifications
+	[self beginEditing];
+	
+	// Re-build string cache
+	NSInteger oldLength = _cache.length;
+	[_cache replaceCharactersInRange:NSMakeRange(0, oldLength) withString:_content];
+	[self edited:NSTextStorageEditedCharacters range:NSMakeRange(0, oldLength) changeInLength:(NSInteger)_content.length - oldLength];
+	
+	// TODO: Update attributes
+	
+	// End coalescing change notifications
+	[self endEditing];
+}
+
+
+#pragma mark - Text Storage Accessors
+
+- (NSString *)string
+{
+	return _cache.string;
+}
+
+- (NSDictionary *)attributesAtIndex:(NSUInteger)location effectiveRange:(NSRangePointer)range
+{
+	return [_cache attributesAtIndex:location effectiveRange:range];
+}
+
+- (void)replaceCharactersInRange:(NSRange)range withString:(NSString *)str
+{
+	NSAssert(self.content != nil, @"Content has not been set!");
+	
+	// Update content and cache -- attributes will be re-computed later
+	[self.content replaceCharactersInRange:range withString:str];
+	[_cache replaceCharactersInRange:range withString:str];
+	
+	// Notify textual change
+	[self edited:NSTextStorageEditedCharacters range:range changeInLength:(NSInteger)str.length - (NSInteger)range.length];
+}
+
+- (void)setAttributes:(NSDictionary *)attrs range:(NSRange)range
+{
+	// Update cache with new attributes only.
+	[_cache setAttributes:attrs range:range];
+	
+	// Notify attribute change
+	[self edited:NSTextStorageEditedAttributes range:range changeInLength:0];
+}
+
+
+#pragma mark - Attribute computation
+
+- (void)processEditing
+{
+	[super processEditing];
+	// TODO: Compute some attributes.
+}
 
 @end
