cef730d020554bb4628164bd21d65a79dd53b7b3
Max Seelemann <max@the-soulmen.com>
2014-04-25 10:24:19 +0200

    Load text editing stack with demo content.

--- TextKitEditor/TKEViewController.m
+++ TextKitEditor/TKEViewController.m
@@ -8,8 +8,20 @@
 
 #import "TKEViewController.h"
 
-@interface TKEViewController ()
+#import "TKECodeString.h"
+#import "TKELayoutManager.h"
+#import "TKETextContainer.h"
+#import "TKETextStorage.h"
 
+
+@interface TKEViewController ()
+{
+	// The code contents to be displayed in the view
+	TKECodeString *_codeString;
+	
+	// Text storage must be held strongly, only the default storage is retained by the text view.
+	TKETextStorage *_textStorage;
+}
 @end
 
 @implementation TKEViewController
@@ -17,13 +29,26 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+	
+	_codeString = [TKECodeString new];
+	_codeString.string = [NSString stringWithContentsOfURL:[NSBundle.mainBundle URLForResource:@"demo" withExtension:@"txt"] usedEncoding:NULL error:NULL];
+	
+	// Create text system
+	_textStorage = [TKETextStorage new];
+	_textStorage.content = _codeString;
+	
+	NSLayoutManager *layoutManager = [TKELayoutManager new];
+	[_textStorage addLayoutManager: layoutManager];
+	
+	NSTextContainer *textContainer = [[TKETextContainer alloc] initWithSize: CGSizeMake(CGFLOAT_MAX, CGFLOAT_MAX)];
+	[layoutManager addTextContainer: textContainer];
+	
+	UITextView *textView = [[UITextView alloc] initWithFrame:CGRectInset(self.view.bounds, 10, 20) textContainer: textContainer];
+	textView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+	textView.translatesAutoresizingMaskIntoConstraints = YES;
+	textView.keyboardDismissMode = UIScrollViewKeyboardDismissModeInteractive;
+	textView.scrollEnabled = YES;
+	[self.view addSubview: textView];
 }
 
 @end
