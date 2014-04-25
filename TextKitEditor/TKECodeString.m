b757735fe1d6a1643dfa37c427e1facd191ad574
Max Seelemann <max@the-soulmen.com>
2014-04-25 10:17:06 +0200

    Implemented dummy code string.

--- TextKitEditor/TKECodeString.m
+++ TextKitEditor/TKECodeString.m
@@ -9,5 +9,42 @@
 #import "TKECodeString.h"
 
 @implementation TKECodeString
+{
+	NSMutableString *_imp;
+}
+
+- (id)init
+{
+    self = [super init];
+    
+    if (self) {
+        _imp = [NSMutableString new];
+    }
+	
+    return self;
+}
+
+
+#pragma mark - String accessors
+
+- (NSUInteger)length
+{
+	return _imp.length;
+}
+
+- (unichar)characterAtIndex:(NSUInteger)index
+{
+	return [_imp characterAtIndex: index];
+}
+
+- (void)getCharacters:(unichar *)buffer range:(NSRange)aRange
+{
+	[_imp getCharacters:buffer range:aRange];
+}
+
+- (void)replaceCharactersInRange:(NSRange)range withString:(NSString *)aString
+{
+	[_imp replaceCharactersInRange:range withString:aString];
+}
 
 @end
